fdca7abf3fae37c217a5cdcae9792e3d8ef15bbd
JakeTurner616 <jake@serverboi.org>
2023-11-22 23:46:26 -0700

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 window.__CONFIG__ = {
   // url must NOT end with a slash
   VITE_CORS_PROXY_URL: "https://movie-web.jake-3c8.workers.dev/",
-  VITE_TMDB_READ_API_KEY: "c91245082d871bdf4ae5debe23196ccd"
+  VITE_TMDB_READ_API_KEY: "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJjOTEyNDUwODJkODcxYmRmNGFlNWRlYmUyMzE5NmNjZCIsInN1YiI6IjY1NWVlZjdmMmIxMTNkMDBjYTRmNDFhNCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.FGg10alBFesUi-Q21NF2ncgUl2Ej2-giyLQdsoUqNMY"
 };
